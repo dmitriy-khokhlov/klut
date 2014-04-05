1a5e0924850fe8ba3a2787489fbef93099c8eb1e
Dmitry Khokhlov <dmitry.hohlov@mail.ru>
2014-04-05 18:07:11 +0400

    Tokens: добавить тип токена и сам токен

---
+++ Klut/Tokens/Token.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klut.Tokens
+{
+    class Token
+    {
+        public String Lexeme { get; set; }
+
+        public TokenType Type { get; set; }
+    }
+}
---
+++ Klut/Tokens/TokenType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klut.Tokens
+{
+    enum TokenType
+    {
+        Identifier,
+        Semicolon,
+        OpenParenthesis,
+        CloseParenthesis,
+        String,
+    }
+}
