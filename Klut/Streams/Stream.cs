eecc464f156bdc028b39f177669d487885186f58
Dmitry Khokhlov <dmitry.hohlov@mail.ru>
2014-04-05 18:08:11 +0400

    Streams: добавить базовый поток, поток исходного кода и поток токенов

---
+++ Klut/Streams/Stream.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klut.Streams
+{
+    class Stream<T>
+    {
+        public delegate void ItemsAddedHandler( Stream<T> sender, int count );
+
+        private Queue<T> queue = new Queue<T>();
+
+        private ItemsAddedHandler onItemAdded;
+
+        public void Send( T item ) {
+            queue.Enqueue( item );
+            onItemAdded( this, 1 );
+        }
+
+        public T Receive() {
+            return queue.Dequeue();
+        }
+
+        public event ItemsAddedHandler OnItemAdded {
+            add { onItemAdded += value; }
+            remove { onItemAdded -= value; }
+        }
+    }
+}
---
+++ Klut/Streams/TokenStream.cs
@@ -0,0 +1,13 @@
+﻿using Klut.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klut.Streams
+{
+    class TokenStream : Stream<Token>
+    {
+    }
+}
