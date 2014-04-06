6af1fa6dece7e8d6792992dde23e2b0d0e5a9ba5
Dmitry Khokhlov <dmitry.hohlov@mail.ru>
2014-04-06 08:20:51 +0400

    Program: добавить простейший запуск конвейера компиляции

--- Klut/Pipeline/CodeSaver.cs
+++ Klut/Pipeline/CodeSaver.cs
@@ -5,18 +5,20 @@ namespace Klut.Pipeline
 {
     class CodeSaver
     {
-
-        public ParseStream InputStream { get; private set; }
+        public CodeStream InputStream { get; private set; }
 
         public string OutputFilePath { get; set; }
 
-        public CodeSaver() {}
+        public CodeSaver( CodeStream inputStream )
+        {
+            InputStream = inputStream;
+        }
 
         private void _handleNewInput( int count )
         {
             for ( ; count > 0; count-- )
             {
-                ParseTree inputTree = InputStream.Receive();
+                byte inputTree = InputStream.Receive();
 
                 // todo: implement handleNewInput
             }
--- Klut/Program.cs
+++ Klut/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Klut
+﻿using Klut.Pipeline;
+
+namespace Klut
 {
     class Program
     {
-        static void Main( string[] args )
+        private static void Main( string[] args )
         {
+            var textProvider = new TextProvider();
+            var codeSaver =
+                new CodeSaver(
+                    new CodeGenerator(
+                        new Optimizer(
+                            new Semanter(
+                                new Parser(
+                                    new Lexer(
+                                        textProvider.OutputStream
+                                        ).OutputStream
+                                    ).OutputStream
+                                ).OutputStream
+                            ).OutputStream
+                        ).OutputStream
+                    );
+            textProvider.ReadFile( "aoe" );
         }
     }
 }
